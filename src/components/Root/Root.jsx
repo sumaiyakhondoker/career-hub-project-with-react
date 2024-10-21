535241bb802f4055a57ea791f6e99d9a61a7fbfe
sumaiyakhondoker <sumaiyakhondoker85@gmail.com>
2024-10-21 19:55:35 +0600

    completed

--- src/components/Root/Root.jsx
+++ src/components/Root/Root.jsx
@@ -1,14 +1,21 @@
 
-import { Outlet } from 'react-router-dom';
+import { Outlet, useNavigation } from 'react-router-dom';
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
 
 const Root = () => {
+
+    const navigation = useNavigation()
+    // console.log(navigation);
     return (
         <div>
          
             <Header></Header>
-            <Outlet></Outlet>
+            {
+                navigation.state === 'loading' ? 
+                <p>Loading...</p>:
+                <Outlet></Outlet>
+            }
             
             <Footer></Footer>
         </div>
