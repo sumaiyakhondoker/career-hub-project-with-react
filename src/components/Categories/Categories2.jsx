81d8587f5ccb86e052863b761c57e60c20294407
sumaiyakhondoker <sumaiyakhondoker85@gmail.com>
2024-10-22 20:53:22 +0600

    updated

--- src/components/Categories/Categories2.jsx
+++ src/components/Categories/Categories2.jsx
@@ -10,6 +10,7 @@ const Categories2 = () => {
         
       <div className="my-20 ">
         <h2 className="text-4xl font-bold my-6 text-center">Categories</h2>
+        <p className="my-6">Explore thousands of job opportunities with all the information you need. Its your future</p>
           <div className="grid grid-cols-4 gap-6 mb-4">
             {
                 categories.map(category => <Category key={category.id} category={category}></Category>)
